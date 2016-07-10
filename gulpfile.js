386ce88e88b16de0f96e583a4677009e266ea67e
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-07-10 13:30:18 -0500

    practice conflict

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ gulp.task('server', function() {
 	var server = gls('./src/index.js');
 	server.start();
 
+// will we get a conflict if i type this? 
+
 	gulp.watch(['gulpfile.js', './src/index.js', './src/controllers/**/*.js'], function() {
 		server.start.bind(server)()
 	});
