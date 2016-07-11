34ac539c8df9a08b5c45abfd721b0778543d1214
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-07-11 14:35:33 -0500

    now correctly identifies album vs artist postings on bandcamp. code is non-dry

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -35,28 +35,78 @@ SearchController.route('/?')
       query: req.body.query,
       page: 1
       }, function(error, results) {
-    if (error) {
-      console.log(error);
-    } 
-    else if (req.body.query.toLowerCase() === results[0].name.toLowerCase()) {
-      //console.log(results);
-      //res.json(results);
-      console.log(results);
-      res.render('searchResult', {
-        query: req.body.query,
-        link: results[0].link, // [0] is band info
-        image: results[1].image // [1] is album info
-      })
-    }
-    else {
-      console.log(results[0].name);
-      console.log(req.body.query);
-      res.render('searchResult', {
-        query: req.body.query + " but maybe that band name is still available"
-      })
-    }
+        if (error) {
+          console.log(error);
+        } 
+
+    /////////  TYPE: ALBUM  /////////    
+        else if (results[0].type === 'album') {
+          if (req.body.query.toLowerCase() === results[0].artist.toLowerCase()) {
+            console.log(results);
+            res.render('searchResult', {
+              query: req.body.query,
+              link: results[0].link, // [0] is band info
+              image: results[0].image // [1] is album info? maybe 
+            })
+          }
+        else {
+          console.log(results);
+          console.log(results[0].name);
+          console.log(req.body.query);
+          res.render('searchResult', {
+            query: req.body.query + " but maybe that band name is still available"
+          })
+        }
+      }
+    /////////  TYPE: ARTIST  /////////    
+        // else if(results[0].type === 'artist') {
+        //   if (req.body.query.toLowerCase() === results[0].name.toLowerCase()) {
+        else if (results[0].type === 'artist') {
+
+          if (req.body.query.toLowerCase() === results[0].name.toLowerCase()) {
+            console.log(results);
+            res.render('searchResult', {
+              query: req.body.query,
+              link: results[0].link, // [0] is band info
+              image: results[0].image // [1] is album info? maybe 
+            })
+          }
+          else {
+            console.log(results);
+            console.log(results[0].name);
+            console.log(req.body.query);
+            res.render('searchResult', {
+            query: req.body.query + " but maybe that band name is still available"
+          })
+        }
+      }
+
+
+    });
   });
-});
+
+
+
+    //  (req.body.query.toLowerCase() === results[0].name.toLowerCase())//|| req.body.query.toLowerCase() === results[0].artist.toLowerCase()) {
+    //   //console.log(results);
+    //   //res.json(results);
+    //   {
+    //   console.log(results);
+    //   res.render('searchResult', {
+    //     query: req.body.query,
+    //     link: results[0].link, // [0] is band info
+    //     image: results[0].image // [1] is album info? maybe 
+    //   })
+    // }
+    // else {
+    //   console.log(results);
+    //   console.log(results[0].name);
+    //   console.log(req.body.query);
+    //   res.render('searchResult', {
+    //     query: req.body.query + " but maybe that band name is still available"
+    //   })
+    // }
+
 
 
 module.exports = SearchController;
