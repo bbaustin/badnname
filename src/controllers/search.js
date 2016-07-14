f2912691088e0fe9fdf7176b549bb7d0aebe2df3
Ben Austin <BenjaminBoydAustin@gmail.com>
2016-07-14 10:39:29 -0500

    Search found property is correctly updating using update mongo method, in both bandcamp and discogs

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -25,12 +25,6 @@ SearchController.route('/getAll')
   });
 
 
-SearchController.route('/userHistory')
-  .get(function(req, res) {
-    res.render('userHistory');
-  });
-
-
 SearchController.route('/?') 
   .get(function(req, res) {
     console.log(req.session.userId, 'this is the session variable')
@@ -53,35 +47,42 @@ SearchController.route('/?')
      // if (!req.timeout) {
      //  next();
      // }
+    Search.create({
+      query: req.body.query,
+      userId: req.session.userId,
+      found: false
+    })
 ///////////  DISCOGS  ////////////
-  // to do tonight ---------> create more handlebars to add discogs info in. 
-    // this will include 'comment,' 'link,' 'etc.' maybe image?
-    // PROBLEM: your "found: true" thing kind of won't work anymore :'( figure it out
-          // FUCK. Yeah, you're not creating a search until it goes through the bandcamp thing. goddammit lol. 
-          // sort of hacky, but can you have two Booleans? like bandcampFound and discogsFound? not terrible :/ 
     var db = new discogs({
-             consumerKey: process.env.PUBLIC_KEY, 
-             consumerSecret: process.env.SECRET_KEY}).database();
-        db.search([req.body.query], ['artist'], function(err, data){
-          console.log(data);
-          if (err) {
-            console.log('your discogs thing has error ' + err);
-          }
-          else if (data.pagination.items===0) { 
-            console.log(req.body.query + " is not found on discogs");
+      consumerKey: process.env.PUBLIC_KEY, 
+      consumerSecret: process.env.SECRET_KEY}).database();
+
+    db.search([req.body.query], ['artist'], function(err, data){
+      console.log(data);
+      if (err) {
+        console.log('your discogs thing has error ' + err);
+      }
+      else if (data.pagination.items===0) { 
+        console.log(req.body.query + " is not found on discogs");
+      }
+      else {
+        for (var i = 0; i < data.results.length; i++) {
+          if (data.results[i].type === 'artist' && data.results[i].title.toLowerCase() === req.body.query.toLowerCase()){
+            console.log('It looks like ' + req.body.query + ' has something on Discogs');
+            Search.findOne({query: req.body.query}, function (err, toUpdate) {
+              toUpdate.update({found:true}, function (err, raw) {
+                if (err) console.log(err);
+                console.log(raw);
+              });
+            })
+            i = data.results.length;
           }
           else {
-            for (var i = 0; i < data.results.length; i++) {
-              if (data.results[i].type === 'artist' && data.results[i].title.toLowerCase() === req.body.query.toLowerCase()){
-                console.log('It looks like ' + req.body.query + ' has something on Discogs');
-                i = data.results.length;
-              }
-              else {
-                console.log('I dont think ' + req.body.query + ' is an existing band name');
-              }
-            }
+            console.log('I dont think ' + req.body.query + ' is an existing band name');
           }
-        })
+        }
+      }
+    })
 ///////////  BANDCAMP SCRAPER  ////////////
     bandcamp.search({
       query: req.body.query,
@@ -94,10 +95,11 @@ SearchController.route('/?')
     /////////  TYPE: ALBUM  /////////    
         else if (results[0].type === 'album') {
           if (req.body.query.toLowerCase() === results[0].artist.toLowerCase()) {
-            Search.create({
-              query: req.body.query,
-              userId: req.session.userId,
-              found: true
+            Search.findOne({query: req.body.query}, function (err, toUpdate) {
+              toUpdate.update({found:true}, function (err, raw) {
+                if (err) console.log(err);
+                console.log(raw);
+              });
             })
             res.render('searchResult', {
               query: req.body.query,
@@ -107,11 +109,6 @@ SearchController.route('/?')
             })
           }
         else {
-          Search.create({
-            query: req.body.query,
-            userId: req.session.userId,
-            found: false
-          })
           res.render('searchResult', {
             query: req.body.query,
             comment: ". That bandname is not registered on Bandcamp!",
@@ -121,14 +118,13 @@ SearchController.route('/?')
         }
       }
     /////////  TYPE: ARTIST  /////////    
-
         else if (results[0].type === 'artist') {
-
           if (req.body.query.toLowerCase() === results[0].name.toLowerCase()) {
-            Search.create({
-              query: req.body.query,
-              userId: req.session.userId,
-              found: true
+            Search.findOne({query: req.body.query}, function (err, toUpdate) {
+              toUpdate.update({found:true}, function (err, raw) {
+                if (err) console.log(err);
+                console.log(raw);
+              });
             })
             res.render('searchResult', {
               query: req.body.query,
@@ -138,11 +134,6 @@ SearchController.route('/?')
             })
           }
           else {
-            Search.create({
-              query: req.body.query,
-              userId: req.session.userId,
-              found: false
-            })
             res.render('searchResult', {
               query: req.body.query,
               link: '/search',
@@ -153,5 +144,109 @@ SearchController.route('/?')
       }
     });
   });
+
+
+
+
+
+
+
+
+
+
+//     var db = new discogs({
+//              consumerKey: process.env.PUBLIC_KEY, 
+//              consumerSecret: process.env.SECRET_KEY}).database();
+//         db.search([req.body.query], ['artist'], function(err, data){
+//           console.log(data);
+//           if (err) {
+//             console.log('your discogs thing has error ' + err);
+//           }
+//           else if (data.pagination.items===0) { 
+//             console.log(req.body.query + " is not found on discogs");
+//           }
+//           else {
+//             for (var i = 0; i < data.results.length; i++) {
+//               if (data.results[i].type === 'artist' && data.results[i].title.toLowerCase() === req.body.query.toLowerCase()){
+//                 console.log('It looks like ' + req.body.query + ' has something on Discogs');
+//                 i = data.results.length;
+//               }
+//               else {
+//                 console.log('I dont think ' + req.body.query + ' is an existing band name');
+//               }
+//             }
+//           }
+//         })
+// ///////////  BANDCAMP SCRAPER  ////////////
+//     bandcamp.search({
+//       query: req.body.query,
+//       page: 1
+//       }, function(error, results) {
+//         if (error) {
+//           console.log(error);
+//           res.send('there was an error with POST /search');
+//         } 
+//     /////////  TYPE: ALBUM  /////////    
+//         else if (results[0].type === 'album') {
+//           if (req.body.query.toLowerCase() === results[0].artist.toLowerCase()) {
+//             Search.create({
+//               query: req.body.query,
+//               userId: req.session.userId,
+//               found: true
+//             })
+//             res.render('searchResult', {
+//               query: req.body.query,
+//               comment: ", but it looks like that band name might be taken.",
+//               link: results[0].link, 
+//               image: '<img src=' + results[0].image + '/>', 
+//             })
+//           }
+//         else {
+//           Search.create({
+//             query: req.body.query,
+//             userId: req.session.userId,
+//             found: false
+//           })
+//           res.render('searchResult', {
+//             query: req.body.query,
+//             comment: ". That bandname is not registered on Bandcamp!",
+//             link: '/search',
+//             image: ''
+//           })
+//         }
+//       }
+//     /////////  TYPE: ARTIST  /////////    
+
+//         else if (results[0].type === 'artist') {
+
+//           if (req.body.query.toLowerCase() === results[0].name.toLowerCase()) {
+//             Search.create({
+//               query: req.body.query,
+//               userId: req.session.userId,
+//               found: true
+//             })
+//             res.render('searchResult', {
+//               query: req.body.query,
+//               link: results[0].link, 
+//               image: '<img src=' + results[0].image + '/>',  
+//               comment: ", but it looks like that band name might be taken."
+//             })
+//           }
+//           else {
+//             Search.create({
+//               query: req.body.query,
+//               userId: req.session.userId,
+//               found: false
+//             })
+//             res.render('searchResult', {
+//               query: req.body.query,
+//               link: '/search',
+//               image: '',
+//               comment: ". That name has not been registered on Bandcamp!"
+//           })
+//         }
+//       }
+//     });
+//   });
 
 module.exports = SearchController;
